aa81accec1c8326a2f59436a8b7a2b29022ccd93
raillane-28 <142820138+raillane-28@users.noreply.github.com>
2023-08-26 15:22:36 -0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+const ordem = {
+    alfabetica: ['Espanha', 'Belgica', 'Alemanha', 'Dourados', 'Camisão'],
+    alfa: function () {
+        this.alfabetica.sort();
+        console.log(this.alfabetica);
+    }
+};
+ordem.alfa();
+
+
+ 
