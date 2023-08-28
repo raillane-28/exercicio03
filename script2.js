14a6b4837ff672d61e0b637edff8a43f722943e2
raillane-28 <142820138+raillane-28@users.noreply.github.com>
2023-08-28 10:56:56 -0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+const desordem = {
+    bagunca: ['Alemanha','Belgica' ,'Camisão','Dourados','Espanha'],
+    desordemAlfa: function () {
+        this.bagunca.reverse();
+        console.log(this.bagunca);
+    }
+};
+desordem.desordemAlfa();
+
