162820f0a3bef4b259f82d15e2164a1980c283cc
raillane-28 <142820138+raillane-28@users.noreply.github.com>
2023-08-28 10:30:50 -0400

    Delete script3.js

--- script3.js
+++
@@ -1,8 +0,0 @@
-const desordem = {
-    bagunca: ['Alemanha','Belgica' ,'Camisão','Dourados','Espanha'],
-    desordemAlfa: function () {
-        this.bagunca.reverse();
-        console.log(this.bagunca);
-    }
-};
-desordem.desordemAlfa();
